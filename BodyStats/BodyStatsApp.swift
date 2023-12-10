cf5f59a548178c2e7f6592aeaa1f32fceefc09e0
Miriam <miriam.lucy@outlook.com>
2023-12-10 08:36:09 +0100

    Initial Commit

---
+++ BodyStats/BodyStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BodyStatsApp.swift
+//  BodyStats
+//
+//  Created by Miriam Renken on 10.12.23.
+//
+
+import SwiftUI
+
+@main
+struct BodyStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
